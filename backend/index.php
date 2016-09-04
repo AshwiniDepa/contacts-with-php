66ab26ea00aef3271b6e3ad4b891b892fe0629b9
Ashwini <achhupande18@gmail.com>
2016-09-04 13:45:55 +0530

    using php as an backend to get contacts

---
+++ backend/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$contactsList = array(array(
+       'name' => 'Ashwini',
+	   'number' => '7842418005'
+    ), [
+	   'name' => 'sucharitha',
+	   'number' => '8990256765'
+	], [
+	   'name' => 'Bindhu',
+	   'number' => '9856453423'
+	], [
+	   'name' => 'Bhavya',
+	   'number' => '786546353'	   
+]);
+
+echo json_encode($contactsList);
